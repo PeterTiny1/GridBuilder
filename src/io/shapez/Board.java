8a0f719ced9dfba6fe1ea6de8448b92ba7a12a50
PeterTiny1 <petertiny1@gmail.com>
2021-02-23 10:18:54 +0000

    Initial commit

---
+++ src/io/shapez/Board.java
@@ -0,0 +1,144 @@
+package io.shapez;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class Board extends JPanel implements ActionListener, MouseWheelListener, KeyListener, MouseMotionListener, MouseListener {
+    private Timer timer;
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int DELAY = 10;
+    private int scale = 40;
+    private int offsetX, offsetY;
+    private ArrayList<Character> pressedKeys = new ArrayList<>();
+    private int gridOffsetX, gridOffsetY;
+    private int previousMX, previousMY;
+
+    public Board() {
+        initBoard();
+    }
+
+    private void initBoard() {
+        addKeyListener(this);
+        addMouseMotionListener(this);
+        addMouseListener(this);
+        setFocusable(true);
+        requestFocusInWindow();
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension size = getSize();
+        g.setColor(Color.BLACK);
+        for (int x = 0; x < size.width; x += scale) {
+            g.drawLine(x + offsetX, 0, x + offsetX, size.height);
+        }
+        for (int y = 0; y < size.height; y += scale) {
+            g.drawLine(0, y + offsetY, size.width, y + offsetY);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (pressedKeys.size() > 0) {
+            for (Character key : pressedKeys) {
+                switch (key) {
+                    case 's':
+                        changeOffset(0, -5);
+                        break;
+                    case 'w':
+                        changeOffset(0, 5);
+                        break;
+                    case 'a':
+                        changeOffset(5, 0);
+                        break;
+                    case 'd':
+                        changeOffset(-5, 0);;
+                        break;
+                }
+            }
+            repaint();
+        }
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        if (e.getWheelRotation() < 0) {
+            scale *= 1.2;
+        } else if (scale > 5) {
+            scale /= 1.2;
+        }
+        changeOffset(0, 0);
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (!pressedKeys.contains(e.getKeyChar())) {
+            pressedKeys.add(e.getKeyChar());
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (pressedKeys.contains(e.getKeyChar())) {
+            pressedKeys.remove((Character) e.getKeyChar());
+        }
+    }
+
+    public void changeOffset(int x, int y) {
+        offsetX += x;
+        gridOffsetX += offsetX / scale;
+        offsetX %= scale;
+        offsetY += y;
+        gridOffsetY += offsetY / scale;
+        offsetY %= scale;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        System.out.println("Mouse dragged to " + e.getX() + " " + e.getY());
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+//        System.out.println("Mouse moved to " + e.getX() + " " + e.getY());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse started clicking at" + e.getX() + " " + e.getY());
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
---
+++ src/io/shapez/Main.java
@@ -0,0 +1,29 @@
+package io.shapez;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() {
+        initUI();
+    }
+
+    private void initUI() {
+        Board board = new Board();
+        JScrollPane scrollPane = new JScrollPane(board);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        board.addMouseWheelListener(board);
+        add(scrollPane);
+        pack();
+        setTitle("Better version of Aurumaker's Grid Builder");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Main main = new Main();
+            main.setVisible(true);
+        });
+    }
+}
