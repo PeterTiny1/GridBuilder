2233483a875f5b64b55c71ed1115c6853de564c3
Aurumaker72 <armkdev72@gmail.com>
2021-02-27 14:17:03 +0100

    easter egg lol

--- src/io/shapez/EntityTutorial.java
+++ src/io/shapez/EntityTutorial.java
@@ -1,6 +1,10 @@
 package io.shapez;
 
+import java.util.Random;
+
 public class EntityTutorial {
+
+    public static Random random = new Random();
 
     public static String GetTitle(Items item){
         if(item == Items.None)return "";
@@ -14,6 +18,8 @@ public class EntityTutorial {
             case Miner:
                 return "Used to extract items";
             case Trash:
+                if(random.nextInt(11) == 10)
+                    return "This is KPOP";
                 return "Used to delete items";
             default:
                 return "";
