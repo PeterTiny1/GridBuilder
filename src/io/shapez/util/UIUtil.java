9c3587e1311083f82ed21e071cfc6b2585f457fe
Aurumaker72 <armkdev72@gmail.com>
2021-03-25 10:01:55 +0100

    More standardization, remove useless class

--- src/io/shapez/util/UIUtil.java
+++ src/io/shapez/util/UIUtil.java
@@ -10,35 +10,36 @@ import io.shapez.ui.TopPanel;
 public class UIUtil {
     public static void updateButtonAppearance() {
         switch (Board.item) {
-            case None:
+            case None -> {
                 BottomPanel.minerButton.setSelected(false);
                 BottomPanel.beltButton.setSelected(false);
                 BottomPanel.trashButton.setSelected(false);
                 BottomPanel.rotatorButton.setSelected(false);
-                break;
-            case Belt:
+            }
+            case Belt -> {
                 BottomPanel.minerButton.setSelected(false);
                 BottomPanel.trashButton.setSelected(false);
                 BottomPanel.rotatorButton.setSelected(false);
                 BottomPanel.beltButton.setSelected(true);
-                break;
-            case Miner:
+            }
+            case Miner -> {
                 BottomPanel.beltButton.setSelected(false);
                 BottomPanel.trashButton.setSelected(false);
                 BottomPanel.rotatorButton.setSelected(false);
                 BottomPanel.minerButton.setSelected(true);
-                break;
-            case Trash:
+            }
+            case Trash -> {
                 BottomPanel.beltButton.setSelected(false);
                 BottomPanel.minerButton.setSelected(false);
                 BottomPanel.rotatorButton.setSelected(false);
                 BottomPanel.trashButton.setSelected(true);
-                break;
-            case Rotator:
+            }
+            case Rotator -> {
                 BottomPanel.minerButton.setSelected(false);
                 BottomPanel.beltButton.setSelected(false);
                 BottomPanel.trashButton.setSelected(false);
                 BottomPanel.rotatorButton.setSelected(true);
+            }
         }
         TopPanel.selectedILabel_Name.setText(EntityTutorial.GetTitle(Board.item));
         TopPanel.selectedILabel_Description.setText(EntityTutorial.GetDescription(Board.item));
