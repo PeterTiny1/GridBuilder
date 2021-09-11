d154d389b39001ffd175e6a6fad6bd402ab10823
PeterTiny1 <56882034+PeterTiny1@users.noreply.github.com>
2021-09-11 21:42:39 +0100

    Update UIUtil.java
    Removed unnecessarily large switch statement

--- src/io/shapez/util/UIUtil.java
+++ src/io/shapez/util/UIUtil.java
@@ -2,6 +2,7 @@ package io.shapez.util;
 
 import io.shapez.core.Resources;
 import io.shapez.Application;
+import io.shapez.core.Tile;
 import io.shapez.game.EntityTutorial;
 import io.shapez.game.platform.SoundManager;
 import io.shapez.ui.BottomPanel;
@@ -12,48 +13,19 @@ import static io.shapez.managers.providers.MiscProvider.*;
 public class UIUtil {
 
     public static String getProcTitle(byte type) {
-        return switch (type) {
-            case OP_SAVE -> moreWndName.concat(" - Saving");
-            case OP_LOAD -> moreWndName.concat(" - Loading");
-            case OP_CLEAR -> moreWndName.concat(" - Clearing");
-            default -> moreWndName;
-            // I forgot Java doesnt have pointers XD
-        };
+        return moreWndName.concat(switch(type) {
+            case OP_SAVE -> " - Saving";
+            case OP_LOAD -> " - Loading";
+            case OP_CLEAR -> " - Clearing";
+            default -> "";
+        });
     }
 
     public static void updateButtonAppearance() {
-        switch (Application.item) {
-            case None -> {
-                BottomPanel.minerButton.setSelected(false);
-                BottomPanel.beltButton.setSelected(false);
-                BottomPanel.trashButton.setSelected(false);
-                BottomPanel.rotatorButton.setSelected(false);
-            }
-            case Belt -> {
-                BottomPanel.minerButton.setSelected(false);
-                BottomPanel.trashButton.setSelected(false);
-                BottomPanel.rotatorButton.setSelected(false);
-                BottomPanel.beltButton.setSelected(true);
-            }
-            case Miner -> {
-                BottomPanel.beltButton.setSelected(false);
-                BottomPanel.trashButton.setSelected(false);
-                BottomPanel.rotatorButton.setSelected(false);
-                BottomPanel.minerButton.setSelected(true);
-            }
-            case Trash -> {
-                BottomPanel.beltButton.setSelected(false);
-                BottomPanel.minerButton.setSelected(false);
-                BottomPanel.rotatorButton.setSelected(false);
-                BottomPanel.trashButton.setSelected(true);
-            }
-            case Rotator -> {
-                BottomPanel.minerButton.setSelected(false);
-                BottomPanel.beltButton.setSelected(false);
-                BottomPanel.trashButton.setSelected(false);
-                BottomPanel.rotatorButton.setSelected(true);
-            }
-        }
+        BottomPanel.beltButton.setSelected(Application.item == Tile.Belt);
+        BottomPanel.minerButton.setSelected(Application.item == Tile.Miner);
+        BottomPanel.rotatorButton.setSelected(Application.item == Tile.Rotator);
+        BottomPanel.trashButton.setSelected(Application.item == Tile.Trash);
         TopPanel.selectedILabel_Name.setText(EntityTutorial.GetTitle(Application.item));
         TopPanel.selectedILabel_Description.setText(EntityTutorial.GetDescription(Application.item));
         TopPanel.selectedILabel_Hotkey.setText(EntityTutorial.GetHotkey(Application.item));
